61b91b3fa6edafdf9903830357f69348a9970db7
panlianghnu <panliang@hnu.edu.cn>
2021-07-16 10:00:10 +0800

    我的页面-用户变量设置

---
+++ src/pages/me/NotLogin.jsx
@@ -0,0 +1,66 @@
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable jsx-quotes */
+import { View, Text } from '@tarojs/components'
+import { AtAvatar, AtButton, AtList, AtListItem } from 'taro-ui'
+import login from '../../static/icons/login.png'
+
+function NotLogin(props) {
+    return (
+        <View>
+            <View style="margin-bottom:50px"></View>
+            <View className="at-row at-row__justify--center">
+                <AtAvatar image={login} circle size="large"></AtAvatar>
+            </View>
+            <View style="margin-top:10px"></View>
+            <View className="at-row">
+                <View className="at-col at-col-12" style="text-align:center">
+                    <Text className="login-text">登陆使用全部功能</Text>
+                </View>
+            </View>
+            <View style="margin-top:20px"></View>
+            <View className="at-row at-row__justify--center">
+                <View className="at-col at-col-6">
+                    <AtButton
+                        type="primary"
+                        full
+                        size="small"
+                        onClick={props.onClick}
+                    >
+                        登陆/注册
+                    </AtButton>
+                </View>
+            </View>
+            <View style="margin-top:40px"></View>
+            <AtList>
+                <AtListItem
+                    title="发票抬头"
+                    arrow="right"
+                    iconInfo={{
+                        size: 20,
+                        color: '#fe5d25',
+                        value: 'money',
+                    }}
+                ></AtListItem>
+                <AtListItem
+                    title="意见反馈"
+                    arrow="right"
+                    iconInfo={{
+                        size: 20,
+                        color: '#fe5d25',
+                        value: 'mail',
+                    }}
+                ></AtListItem>
+                <AtListItem
+                    title="联系我们"
+                    arrow="right"
+                    iconInfo={{
+                        size: 20,
+                        color: '#fe5d25',
+                        value: 'phone',
+                    }}
+                ></AtListItem>
+            </AtList>
+        </View>
+    )
+}
+export { NotLogin }
--- src/pages/me/me.jsx
+++ src/pages/me/me.jsx
@@ -1,67 +1,25 @@
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable jsx-quotes */
 import { Component } from 'react'
-import { View, Text } from '@tarojs/components'
-import { AtAvatar, AtButton, AtList, AtListItem } from 'taro-ui'
-import login from '../../static/icons/login.png'
 import './me.css'
+import { NotLogin } from './NotLogin'
+import { getGlobalData } from '../../global'
 
 export default class extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLogin: false,
+        }
+    }
+
+    componentDidShow() {
+        const userName = getGlobalData('userName')
+        if (userName === '') console.log('未登陆')
+        else console.log('用户名：', userName)
+    }
+
     render() {
-        return (
-            <View>
-                <View style="margin-bottom:50px"></View>
-                <View className="at-row at-row__justify--center">
-                    <AtAvatar image={login} circle size="large"></AtAvatar>
-                </View>
-                <View style="margin-top:10px"></View>
-                <View className="at-row">
-                    <View
-                        className="at-col at-col-12"
-                        style="text-align:center"
-                    >
-                        <Text className="login-text">登陆使用全部功能</Text>
-                    </View>
-                </View>
-                <View style="margin-top:20px"></View>
-                <View className="at-row at-row__justify--center">
-                    <View className="at-col at-col-6">
-                        <AtButton type="primary" full size="small">
-                            登陆/注册
-                        </AtButton>
-                    </View>
-                </View>
-                <View style="margin-top:40px"></View>
-                <AtList>
-                    <AtListItem
-                        title="发票抬头"
-                        arrow="right"
-                        iconInfo={{
-                            size: 20,
-                            color: '#fe5d25',
-                            value: 'money',
-                        }}
-                    ></AtListItem>
-                    <AtListItem
-                        title="意见反馈"
-                        arrow="right"
-                        iconInfo={{
-                            size: 20,
-                            color: '#fe5d25',
-                            value: 'mail',
-                        }}
-                    ></AtListItem>
-                    <AtListItem
-                        title="联系我们"
-                        arrow="right"
-                        iconInfo={{
-                            size: 20,
-                            color: '#fe5d25',
-                            value: 'phone',
-                        }}
-                    ></AtListItem>
-                </AtList>
-            </View>
-        )
+        if (!this.state.isLogin) return <NotLogin></NotLogin>
     }
 }
