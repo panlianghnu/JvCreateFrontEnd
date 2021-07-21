15567d60f6a901b5892456f822c3a61597082dcf
panlianghnu <panliang@hnu.edu.cn>
2021-07-21 12:54:28 +0800

    重构发明页

--- src/pages/invention/invention.jsx
+++ src/pages/invention/invention.jsx
@@ -4,7 +4,8 @@ import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import Taro, { getCurrentInstance } from '@tarojs/taro'
 import axios from 'taro-axios'
-import { AtCard } from 'taro-ui'
+import { AtIcon } from 'taro-ui'
+import './invention.css'
 
 // 这是专利页面
 export default class Ivention extends Component {
@@ -56,31 +57,50 @@ export default class Ivention extends Component {
     render() {
         const renderList = this.state.iventions.map((item, index) => {
             return (
-                <View key={item.name}>
-                    <AtCard
-                        title={item.name}
-                        onClick={this.onClickCard.bind(this, item)}
-                    >
-                        <View className="at-article__p">
-                            <Text>
-                                序号：{index + '\n'}
-                                申请日：{item.filingDate + '\n'}
-                                发明人：{item.inventionPerson + '\n'}
-                                法律状态：
-                            </Text>
-                            <Text style={this.getStatusColor(item)}>
-                                {item.status + '\n'}
-                            </Text>
+                <View
+                    key={item.id}
+                    className="listItem"
+                    hoverClass="hoverList"
+                    onClick={this.onClickCard.bind(this, item)}
+                >
+                    <View className="at-row at-row__align--center">
+                        <View className="at-col">
+                            <View className="at-row at-row__align--center">
+                                <View className="index at-col at-col-1 ">
+                                    {index}
+                                </View>
+                                <View style="margin-left:10px"></View>
+                                <View className="inventionName at-col at-col--wrap">
+                                    {item.name}
+                                </View>
+                            </View>
+                            <View className="at-row at-row__align--center">
+                                <View className="at-col at-col-12">
+                                    <View className="note">
+                                        <Text>
+                                            申请日：{item.filingDate + '\n'}
+                                            发明人：
+                                            {item.inventionPerson + '\n'}
+                                            法律状态：
+                                        </Text>
+                                        <Text style={this.getStatusColor(item)}>
+                                            {item.status + '\n'}
+                                        </Text>
+                                    </View>
+                                </View>
+                            </View>
                         </View>
-                    </AtCard>
-                    <View style="margin-bottom:20px"></View>
+                        <View className="at-row__justify--end at-col at-col-1">
+                            <AtIcon value="chevron-right" size="20" />
+                        </View>
+                    </View>
                 </View>
             )
         })
         return (
             <View>
                 <View style="margin-bottom:10px"></View>
-                <View>{renderList}</View>
+                <View className="inventionList">{renderList}</View>
                 <View style="margin-top:10px">{'\0'}</View>
             </View>
         )
