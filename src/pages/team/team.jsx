316f9b1b7fe08eddd4178f806ea0fc453c8f2236
liaoxuan6 <2583771733@qq.com>
2021-07-29 11:08:58 +0800

    展示团队人员的全部介绍

--- src/pages/team/team.jsx
+++ src/pages/team/team.jsx
@@ -1,13 +1,16 @@
-import { View } from "@tarojs/components";
+import { View,Text} from "@tarojs/components";
 import { Current } from "@tarojs/taro";
 import { Component } from "react";
 import { AtAvatar, AtTextarea } from 'taro-ui';
 import Taro from "@tarojs/taro";
+import { AtModal, AtModalHeader, AtModalContent, AtModalAction } from "taro-ui"
 import './team.css';
 
 export default class Team extends Component{
     constructor(props){
-        super(props)
+        super(props);
+        this.handleClick1 = this.handleClick1.bind(this);
+        this.handleClose  = this.handleClose .bind(this);
         this.state = {
             team : [
                 {
@@ -16,8 +19,12 @@ export default class Team extends Component{
                     picture:'asdzz',
                     introduction:'asdzz'
                 }
-            ]
+            ],
+            flag:false,
+            // 
+            index:0
         }
+        
     }
     componentWillMount() {}
 
@@ -33,7 +40,6 @@ export default class Team extends Component{
             'content-type': 'application/json' // 默认值
             },
             success:function(res){
-                console.log(res.data)
                 th.setState({
                     team : res.data.team
                 })
@@ -54,10 +60,26 @@ export default class Team extends Component{
         this.componentDidMount()
         Taro.stopPullDownRefresh()
     }
+
+    handleClick1(e) {
+        this.setState(
+            {
+                flag: true,
+                index: e
+            }
+        );
+    }
+    
+    handleClose() {
+        this.setState({flag: false});
+    }
+    
     render(){
+        let flag = this.state.flag
+        //let index = this.state.index
         const teams = this.state.team.map(item => {
             return(
-                <View className="teamList" key={item.id}>
+                <View className="teamList" key={item.id} onClick={() => this.handleClick1(item.id)} >
                     <View className='at-row at-row__align--center'>
                         <View className="at-col at-col__offset-1 at-col-1 at-col--auto">
                             <AtAvatar
@@ -68,18 +90,35 @@ export default class Team extends Component{
                         </View>
                     
                         <View className="at-col at-col__offset-1">
-                            <View className="name">{item.name}</View>
+                            <View className="name">
+                                <Text decode='true'>
+                                    {item.name}&emsp;{item.position}
+                                </Text>
+                            </View>
                             <View className='intro'>
                                 <View className='at-col--wrap' >{item.introduction}</View>
                             </View>
-                            
                         </View>
                     </View>
                 </View>
             )
         } )
+        let modal = <AtModal isOpened={this.state.flag} closeOnClickOverlay = 'true' onClose={ this.handleClose }>
+                        <AtModalHeader>{this.state.team[this.state.index].name}</AtModalHeader>
+                        <AtModalContent >
+                            <View style="text-align: justify;">
+                                {this.state.team[this.state.index].introduction}
+                            </View>
+                        </AtModalContent>
+                    </AtModal>
         return(
-            <View>{<View>{teams}</View>}</View>
+            <View>
+                <View>{teams}</View>
+                {modal}
+            </View>
+            
         )
+        
+        
     }
 }
