9a3de43084f351a8bc65c0cdb6574ab5543949df
liaoxuan6 <2583771733@qq.com>
2021-08-19 17:22:17 +0800

    wxLogin

--- src/global.jsx
+++ src/global.jsx
@@ -3,9 +3,18 @@ const globalData = {
     userAvatar: '',
     token: '',
 }
+var isLogin = false
+
 export function setGlobalData(key, val) {
     globalData[key] = val
 }
 export function getGlobalData(key) {
     return globalData[key]
+}
+export function setLoginState(val){
+    isLogin = val;
+}
+
+export function getLoginState(){
+    return isLogin;
 }
